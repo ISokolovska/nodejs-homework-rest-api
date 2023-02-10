dfe6a73a3adc35dc7090714e216d306d44a6063c
Iuliia Sokolovska <uu.sokil@gmail.com>
2023-02-10 18:59:04 +0100

    test2

--- tests/signup.test.js
+++ tests/signup.test.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+
 const { signUp } = require("../controller/userController");
 
 const email = "a.sokil@gmail.com";
@@ -6,7 +7,26 @@ const password = "test123";
 const payload = { id: 123456, username: "Larson" };
 const secret = "secret word";
 
-test("return token after signup", () => {
-  const { token } = signUp({ email, password });
-  expect(token).toString().includes(jwt.sign(payload, secret));
+// test("should return 201 Status", () => {
+//   // const res = signUp({ email, password });
+//   // expect(res).toBe(201);
+
+// });
+describe("signUp", () => {
+  test("should return 201 Status", () => {
+    const drink = jest.fn();
+    drinkAll(drink, "lemon");
+    expect(drink).toHaveBeenCalled();
+  });
 });
+
+// test("should return token after signup", () => {
+//   const { token } = signUp({ email, password });
+//   expect(token).toString().includes(jwt.sign(payload, secret));
+// });
+
+// test("should return user with email and subscription after signup", () => {
+//   const user = signUp({ email, password });
+//   expect(user.email).toString();
+//   expect(user.subscription).toString();
+// });
